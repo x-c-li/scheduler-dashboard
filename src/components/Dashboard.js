452d3fb2af3b882e7d2bc15712fa907bc5eaa5e5
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-12 13:47:43 -0400

    Add the fake data to the top of the src/components/Dashboard.js file.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,9 +4,32 @@ import classnames from "classnames";
 
 import Loading from "./Loading";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   
-  state = { loading: true };
+  state = { loading: false };
 
   render() {
     const dashboardClasses = classnames("dashboard");
