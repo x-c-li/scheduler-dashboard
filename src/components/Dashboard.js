3ed5c7715b17889080e0c391246e31644c9d557a
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-12 14:39:01 -0400

    Use the this.state.focused value to filter panel data before converting it to components.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -43,13 +43,17 @@ class Dashboard extends Component {
       return <Loading />
     }
     
-    const panels = data.map(panel => (
-      <Panel 
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+        <Panel 
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
     ));
 
     return <main className={dashboardClasses}>{panels}</main>;
