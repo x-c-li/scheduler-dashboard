f8959bc330b646c9cfeb2f7404e2c43da8b9ca3d
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-13 11:45:58 -0400

    Add an onClick handler to the Panel component root element. Call the onSelect function with the id of the panel.

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { label, value } = this.props;
+    const { id, onSelect, label, value } = this.props;
 
     return (
       <section
-        className="dashboard__panel"
+        className="dashboard__panel" onClick={event => onSelect(id)}
       >
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
