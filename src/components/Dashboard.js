3f2040aecc8d144762f174764f7dc02abf95ca48
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-13 11:45:10 -0400

    add selectPanel as an input to Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -63,6 +63,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={this.selectPanel}
         />
       ));
 
