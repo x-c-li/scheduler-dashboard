e2f0ba7e3f1057546afa71d32c83435cfa68d3ea
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-13 12:05:32 -0400

    Change the selectPanel function to set the value of focused back to null if the value of focused is currently set to a panel.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -36,9 +36,9 @@ class Dashboard extends Component {
   };
 
   selectPanel = (id) => {
-    this.setState({
-     focused: id
-    });
+    this.setState(previousState => ({
+     focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
